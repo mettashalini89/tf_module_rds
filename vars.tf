ca028b553e59ba5946da35e731337f656d18369b
mettashalini89@gmail.com <CatMatDog@123>
2023-03-22 23:50:04 -0600

    mysql as aws service rds, adding subnet groups

---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "env" {}
+variable "engine" {}
+variable "backup_retention_period" {}
+variable "preferred_backup_window" {}
+
+variable "engine_version" {}
+variable "subnet_ids" {}
+variable "tags" {}
+
+variable "database_name" {
+  default = "dummy"
+}
